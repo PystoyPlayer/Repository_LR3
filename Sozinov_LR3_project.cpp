84b31ddbc04351a9534fa422a535ca24fb482742
Sozinov Ilya <ilya@sozinov.com>
2025-03-17 22:32:26 +0300

    Create void main

---
+++ Sozinov_LR3_project.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double EnterX() {
+    // Ввод вещественного числа x
+}
+
+double EnterY() {
+    // Ввод вещественного числа y
+}
+
+double EnterZ() {
+    // Ввод вещественного числа z
+}
+
+void IntSumPart(double x, double y, double z) {
+    // Найти целую часть суммы этих 3-х чисел
+}
+
+void FracGeomAvg(double x, double y, double z) {
+    // Найти дробную часть среднего геометрического модулей этих 3-х чисел
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    double x = 0, y = 0, z = 0;
+    int choice;
+
+    do {
+        cout << "\n===== Меню =====\n";
+        cout << "1. Ввести вещественное число x\n";
+        cout << "2. Ввести вещественное число y\n";
+        cout << "3. Ввести вещественное число z\n";
+        cout << "4. Найти целую часть суммы чисел\n";
+        cout << "5. Найти дробную часть среднего геометрического модулей чисел\n";
+        cout << "0. Выход\n";
+        cout << "Выберите пункт меню: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                x = EnterX();
+                break;
+            case 2:
+                y = EnterY();
+                break;
+            case 3:
+                z = EnterZ();
+                break;
+            case 4:
+                IntSumPart(x, y, z);
+                break;
+            case 5:
+                FracGeomAvg(x, y, z);
+                break;
+            case 0:
+                cout << "Завершение работы приложения." << endl;
+                break;
+            default:
+                cout << "Неверный ввод, повторите выбор." << endl;
+        }
+    } while (choice != 0);
+
+    return 0;
+}
